da25798eb0eb29aaeee6034d3bbd3ec2630daa53
azureossdemo <ossdemo454242@outlook.com>
2024-07-10 10:42:38 +0000

    first commit

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,20 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const homeText = process.env.NEXT_PUBLIC_HOME_TEXT || "default";
+
   return (
     <div className={styles.container}>
       <Head>
+        
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <p>{homeText}</p>
       </Head>
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome 2 <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
